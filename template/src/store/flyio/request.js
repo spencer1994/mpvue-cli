6787ba308411be8e777c7b872d5829566d3d44c8
douya huang <442604117@qq.com>
2019-01-02 12:33:47 +0800

    Update request.js
    修改多个接口请求时loading的展示

--- template/src/store/flyio/request.js
+++ template/src/store/flyio/request.js
@@ -3,7 +3,6 @@ import API from './apiUrl'
 import Config from './config'
 // 异常情况的错误处理
 const errorFunction = (_tipConfig, err) => {
-  clearFunction(_tipConfig)
   // 如果有异常需要提示
   if (!_tipConfig.errorAction && _tipConfig.isErrorDefaultTip) {
     Config.resError.tipShow(err)
@@ -11,13 +10,14 @@ const errorFunction = (_tipConfig, err) => {
   throw (err)
 }
 
-let promises = [] // 接收接口请求的promise数组
-let loadingTimer = null // loading的定时器
+let showLoadingTimer = null // 显示loading的定时器
+let hideLoadingTimer = null // 隐藏loading的定时器
 
-const clearFunction = (_tipConfig) => {
-  promises = [] // 所有请求完后清除promise数组
-  wx.hideLoading() // 请求完成则隐藏loading
-  clearTimeout(loadingTimer) // 当请求在xxxms内完成则直接清除loading计时器
+const hideLoading = (tipConfig) => {
+  hideLoadingTimer = setTimeout(() => {
+    tipConfig.isLoading && wx.hideLoading() // 请求完成则隐藏loading
+  }, Config.loading.limitTime + 50)
+  clearTimeout(showLoadingTimer) // 当请求在xxxms内完成则直接清除loading计时器
 }
 
 // 接口请求封装函数
@@ -27,31 +27,27 @@ const handleRequest = (url = '', data = {}) => {
     let flyio = Flyio.request(_url, data, {...Config.flyConfig, ...flyConfig})
 
     let _tipConfig = {...Config.reqConfig, ...tipConfig}
-    // 开启loading
-    clearTimeout(loadingTimer) // 多个接口时需要清除上一个loading
-    loadingTimer = setTimeout(() => {
-      _tipConfig.isLoading && wx.showLoading({
-        title: '加载中',
-        mask: true
-      })
-    }, Config.loading.limitTime)
-
-    // 计算当前的promise是否全部加载完成
-    promises.push(flyio)
-    Flyio.all(promises).then(data => {
-      if (data.length !== promises.length) return
-      clearFunction(_tipConfig)
-    }).catch(() => {
-      clearFunction(_tipConfig)
-    })
+    if (_tipConfig.isLoading) {
+      // 开启loading
+      clearTimeout(showLoadingTimer) // 多个接口时需要清除上一个的显示loading
+      clearTimeout(hideLoadingTimer) // 多个接口时需要清除上一个的隐藏loading
+      showLoadingTimer = setTimeout(() => {
+        _tipConfig.isLoading && wx.showLoading({
+          title: '加载中',
+          mask: true
+        })
+      }, Config.loading.limitTime)
+    }
     // 返回请求
     return flyio.then((res) => {
+      hideLoading(_tipConfig)
       if (res.returnCode === '0') {
         return res
       } else {
         errorFunction(_tipConfig, res)
       }
     }).catch(err => {
+      hideLoading(_tipConfig)
       errorFunction(_tipConfig, err)
     })
   }

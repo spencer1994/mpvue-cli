a63108972d5c6b53804a132629f6d47d6cbecca5
douya huang <442604117@qq.com>
2019-01-21 09:42:11 +0800

    Update App.vue
    去除app.vue中的动画

--- template/src/App.vue
+++ template/src/App.vue
@@ -22,11 +22,4 @@ export default {
   padding: 200rpx 0;
   box-sizing: border-box;
 }
-/* this rule will be remove */
-* {
-  transition: width 2s;
-  -moz-transition: width 2s;
-  -webkit-transition: width 2s;
-  -o-transition: width 2s;
-}
 </style>
